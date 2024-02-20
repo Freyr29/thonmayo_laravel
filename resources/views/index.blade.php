3414e73664e141755b8a3451b446ea46b00dce32
pwnii <ineed@protonmail.com>
2024-02-20 15:07:51 +0100

    A fixer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
+<header>
+        <div class="titre" style="font-family: 'Montserrat', sans-serif;">ThonMayo</div>
+        <div class="panier"><a class="cadi" href=""><i class="fa-solid fa-cart-shopping"></i></a></div>
+    </headers>
 <head>
     <meta charset="UTF-8">
     <title>ThonMayo</title>
@@ -11,57 +15,6 @@
     </style>
 </head>
 <body>
-    <header>
-        <div class="titre" style="font-family: 'Montserrat', sans-serif;">ThonMayo</div>
-        <div class="panier"><a class="cadi" href=""><i class="fa-solid fa-cart-shopping"></i></a></div>
-    </header>
-    <nav id="navbar">
-        <ul class="navbar-items flexbox-col" style="margin-top: 20px;">
-            <li class="navbar-item flexbox-left">
-                <a href="{{ route('accueil') }}" class="navbar-item-inner flexbox-left">
-                    <div class="navbar-item-inner-icon-wrapper flexbox">
-                        <i class="fa-solid fa-house-chimney"></i>
-                    </div>
-                    <span class="link-text">ThonMayo</span>
-                </a>
-            </li>
-        </ul> 
-        <ul class="navbar-items flexbox-col">
-            <li class="navbar-logo flexbox-left">
-            </li>
-            <li class="navbar-item flexbox-left">
-                <a href="{{ route('menus') }}" class="navbar-item-inner flexbox-left">
-                    <div class="navbar-item-inner-icon-wrapper flexbox">
-                        <i class="fa-solid fa-utensils"></i>
-                    </div>
-                    <span class="link-text">Menus</span>
-                </a>
-            </li>
-            <li class="navbar-item flexbox-left">
-                <a href="{{ route('sandwichs') }}" class="navbar-item-inner flexbox-left">
-                    <div class="navbar-item-inner-icon-wrapper flexbox">
-                        <i class="fa-solid fa-burger"></i>
-                    </div>
-                    <span class="link-text">Sandwichs</span>
-                </a>
-            </li>
-            <li class="navbar-item flexbox-left">
-                <a href="{{ route('boissons') }}" class="navbar-item-inner flexbox-left">
-                    <div class="navbar-item-inner-icon-wrapper flexbox">
-                        <i class="fa-solid fa-glass-water"></i>
-                    </div>
-                    <span class="link-text">Boissons</span>
-                </a>
-            </li>
-            <li class="navbar-item flexbox-left">
-                <a href="{{ route('snacks') }}" class="navbar-item-inner flexbox-left">
-                    <div class="navbar-item-inner-icon-wrapper flexbox">
-                        <i class="fa-solid fa-cookie-bite"></i>
-                    </div>
-                    <span class="link-text">Snacks</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
+@include('layout.menus') 
 </body>
 </html>
