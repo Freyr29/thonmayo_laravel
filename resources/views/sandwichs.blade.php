0dfce54538ffdb66583bbbebcda3f414f07b6d71
KilWrBen <benoit.mesmay@gmail.com>
2024-02-21 14:54:46 +0100

    css 3 sandwich par ligne + sandwich add

--- resources/views/sandwichs.blade.php
+++ resources/views/sandwichs.blade.php
@@ -63,9 +63,9 @@
         </ul>
     </nav>
     <div class="sand">
-    <div class="sand2">
+    <div class="sand2" >
         <div class="col-span-12 grid grid-cols-3 gap-4">
-            <div class="card min-w-0 max-w-md" style="display: flex; gap: 10%;">
+            <div class="card min-w-0 max-w-md" style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 10%;">
                 @foreach ($sandwichs as $sandwich)
                     <div class="bg-white shadow-md rounded-lg overflow-hidden w-72">
                         <!-- Utiliser l'URL de l'image depuis la base de données -->
