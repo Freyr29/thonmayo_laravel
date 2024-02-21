da19fede723b7561f783ed11b642ead78df0035b
KilWrBen <benoit.mesmay@gmail.com>
2024-02-21 16:21:57 +0100

    snack add

--- database/migrations/2024_02_06_142424_snacks.php
+++ database/migrations/2024_02_06_142424_snacks.php
@@ -23,7 +23,11 @@ class Snacks extends Migration
         DB::statement('INSERT INTO snacks VALUES(2,"Patates sautées maisons","pomme de terre françaises","https://static.750g.com/images/1200-675/aeb1c574fad00aec34a19a476eaf68fa/la-cuisson.jpg", 4.23)');
         DB::statement('INSERT INTO snacks VALUES(3,"Salade maison","Salade à la tomate et à la salade","https://media.soscuisine.com/images/recettes/large/331.jpg", 1.69)');
         DB::statement('INSERT INTO snacks VALUES(4,"Chips Lays","Chips de luxe" , "https://www.lays.fr/images/default-source/products/packshots/lays-natural.png?sfvrsn=2", 0.50)');
-        DB::statement('INSERT INTO snacks VALUES(5,"Sauce barbecue","Sauce barbecue maison", "https://static.750g.com/images/1200-630/bd34ac3c244d9779d4b25fd804e4d306/adobestock-271099773.jpeg", 2)');
+        DB::statement('INSERT INTO snacks VALUES(5,"Sauce barbecue","Sauce barbecue maison", "https://static.750g.com/images/1200-630/bd34ac3c244d9779d4b25fd804e4d306/adobestock-271099773.jpeg", 1.12)');
+        DB::statement('INSERT INTO snacks VALUES(6,"Sauce béarnaise","Sauce béarnaise maison", "https://www.soreal.fr/images/medias/recettes_sauce-cuisinee-bearnaise-384x3404x.jpg", 1.34)');
+        DB::statement('INSERT INTO snacks VALUES(7,"Sauce samouraï","Sauce samouraï maison", "https://www.soreal.fr/images/medias/recettes_sauce-samourai-384x3404x.jpg", 1.46)');
+
+
     }
 
     /**
