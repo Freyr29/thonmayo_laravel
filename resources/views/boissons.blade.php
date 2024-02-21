4e4a8af55267507c24c5c88587dd5a4364769dbe
pwnii <ineed@protonmail.com>
2024-02-21 15:00:30 +0100

    Adding CSS layout

--- resources/views/boissons.blade.php
+++ resources/views/boissons.blade.php
@@ -14,54 +14,7 @@
     <header>
         <div class="titre" style="font-family: 'Montserrat', sans-serif;">Les Boissons de ThonMayo</div>
     </header>
-    <nav id="navbar">
-        <ul class="navbar-items flexbox-col" style="margin-top: 20px;">
-            <li class="navbar-item flexbox-left">
-                <a href="{{ route('accueil') }}" class="navbar-item-inner flexbox-left">
-                    <div class="navbar-item-inner-icon-wrapper flexbox">
-                        <i class="fa-solid fa-house-chimney"></i>
-                    </div>
-                    <span class="link-text">ThonMayo</span>
-                </a>
-            </li>
-        </ul>
-        <ul class="navbar-items flexbox-col">
-            <li class="navbar-logo flexbox-left">
-            </li>
-            <li class="navbar-item flexbox-left">
-                <a href="{{ route('menus') }}" class="navbar-item-inner flexbox-left">
-                    <div class="navbar-item-inner-icon-wrapper flexbox">
-                        <i class="fa-solid fa-utensils"></i>
-                    </div>
-                    <span class="link-text">Menus</span>
-                </a>
-            </li>
-            <li class="navbar-item flexbox-left">
-                <a href="{{ route('sandwichs') }}" class="navbar-item-inner flexbox-left">
-                    <div class="navbar-item-inner-icon-wrapper flexbox">
-                        <i class="fa-solid fa-burger"></i>
-                    </div>
-                    <span class="link-text">Sandwichs</span>
-                </a>
-            </li>
-            <li class="navbar-item flexbox-left">
-                <a href="{{ route('boissons') }}" class="navbar-item-inner flexbox-left">
-                    <div class="navbar-item-inner-icon-wrapper flexbox">
-                        <i class="fa-solid fa-glass-water"></i>
-                    </div>
-                    <span class="link-text">Boissons</span>
-                </a>
-            </li>
-            <li class="navbar-item flexbox-left">
-                <a href="{{ route('snacks') }}" class="navbar-item-inner flexbox-left">
-                    <div class="navbar-item-inner-icon-wrapper flexbox">
-                        <i class="fa-solid fa-cookie-bite"></i>
-                    </div>
-                    <span class="link-text">Snacks</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
+    @include('layout.layout_menu')
     <div style="display: flex; justify-content: center; align-items: flex-start; height: 90vh;">
     <div style="width: 100%; max-width: 1200px; margin-top: 5%; margin-left: 10%;">
         <div class="col-span-12 grid grid-cols-3 gap-4">
