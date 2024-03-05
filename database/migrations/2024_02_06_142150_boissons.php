2c3d220f5dc4518a4f673b2088195cf3960d7b79
Jilox <120713806+Jilox@users.noreply.github.com>
2024-03-05 15:16:11 +0100

    drink add4

--- database/migrations/2024_02_06_142150_boissons.php
+++ database/migrations/2024_02_06_142150_boissons.php
@@ -23,8 +23,8 @@ class Boissons extends Migration
         DB::statement('INSERT INTO boissons VALUES(1, "Coca Cola", 1.58, "image/coca-cola-fr-slim-33.jpg", "Canette", 33)');
         DB::statement('INSERT INTO boissons VALUES(2, "Coca Cola Zéro", 1.94, "image/coca-cola-zero-33-cl.jpg", "Canette", 33)');
         DB::statement('INSERT INTO boissons VALUES(3, "Coca Cola Chery", 1.97, "image/coca-cola-chery-33cl.jpg", "Canette", 33)');
-        DB::statement('INSERT INTO boissons VALUES(4, "Ice Tea", 1.76, "image/ice-tea-33cl.jpg", "Canette", 33)');
-        DB::statement('INSERT INTO boissons VALUES(5, "Oasis", 1.76, "image/oasis-33cl.jpg", "Canette", 33)');
+        DB::statement('INSERT INTO boissons VALUES(4, "Ice Tea", 1.76, "image/ice-tea-33cl.png", "Canette", 33)');
+        DB::statement('INSERT INTO boissons VALUES(5, "Oasis", 1.76, "image/oasis-33cl.png", "Canette", 33)');
     }
     /**
      * Reverse the migrations.
